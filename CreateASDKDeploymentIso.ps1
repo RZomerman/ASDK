35f9b37ed6f97a13e87e23fd7ac673a24966417d
Roelf Zomerman <rozome@microsoft.com>
2018-06-10 17:01:23 +0400

    1

--- CreateASDKDeploymentIso.ps1
+++ CreateASDKDeploymentIso.ps1
@@ -27,7 +27,6 @@
 #$TargetDirectory='d:\winpe_amd81'
 $version="201806105"
 
-
 $TargetBatchFile=($env:TEMP + '\PreparewinPE.bat')
 $ClosingISOBatchFile=($env:TEMP + '\PrepareISO.bat')
 function Write-LogMessage {
@@ -37,17 +36,24 @@ function Write-LogMessage {
           [string]$SystemName = "PRE-ASDK",
           
           [parameter(Mandatory = $false)]
-          [string]$Message = ''
-      )
+          [string]$Message = '',
+
+          [parameter(Mandatory = $false)]
+          [boolean]$NoNewLine
+    )
   
     BEGIN {}
     PROCESS {
       Write-Verbose "Writing log message"
       # Function for displaying formatted log messages.  Also displays time in minutes since the script was started
-      write-host (Get-Date).ToShortTimeString() -ForegroundColor Cyan -NoNewline;
-      write-host ' - [' -ForegroundColor White -NoNewline;
-      write-host $systemName -ForegroundColor Yellow -NoNewline;
-      write-Host "]::$($message)" -ForegroundColor White;
+      If ($NoNewLine) {
+        write-Host "($message)" -ForegroundColor White -NoNewline;
+      }Else{
+        write-host (Get-Date).ToShortTimeString() -ForegroundColor Cyan -NoNewline;
+        write-host ' - [' -ForegroundColor White -NoNewline;
+        write-host $systemName -ForegroundColor Yellow -NoNewline;
+        write-Host "]::$($message)" -ForegroundColor White;
+      }
     }
     END {}
   } 
@@ -222,7 +228,18 @@ If (!(test-path "C:\Program Files (x86)\Windows Kits\10\Assessment and Deploymen
     Write-host ""
 
     
-} else {
+}else{
+#windows ADK was found
+    Write-LogMessage -Message "Windows ADK found"
+}
+
+#Validating input directory
+If ($TargetDirectory.Contains(" ")) {
+    Write-LogMessage -Message "Input contains spaces, trimming"
+    $TargetDirectory=$TargetDirectory.Replace(" ","")
+    Write-LogMessage -Message "Directory to be used: $TargetDirectory"
+}
+
 #Creating a copy of the Deployment Toolkit cmd start script so we can add the copype.cmd to it when starting
     copy-item "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat" $TargetBatchFile 
         $batchfile = Get-Content $TargetBatchFile
@@ -231,24 +248,26 @@ If (!(test-path "C:\Program Files (x86)\Windows Kits\10\Assessment and Deploymen
     Set-Content -Value $batchfile -Path $TargetBatchFile -Force
 
 #Starting the Deployment Toolkit CMD from the created batchfile - this will automatically start the copype.cmd script
-        If (!(test-path ($TargetDirectory + '\media'))) {       
+    Write-LogMessage -Message "creating WinPE on $TargetDirectory"
+    If (!(test-path $TargetDirectory)) {       
             Write-LogMessage -Message "Creating base WinPE image"
             Start-Process 'C:\WINDOWS\system32\cmd.exe' -argumentlist "/k $TargetBatchFile" -Verb runAs -WindowStyle Minimized
-            }
-    } else {
-        Write-LogMessage -Message "Windows Deployment tools not found!" -ForegroundColor red
-        exit
+    }else{
+        Write-LogMessage -Message "Target Directory already exists"
+#exit
     }
 
 #Need to wait for the copype.cmd to be completed - last file seems to be Media\zh-tw\bootmgt.efi.mui
-    $MonitorredFile=($TargetDirectory + '\Media\zh-tw\bootmgr.efi.mui')
+$MonitorredFile=($TargetDirectory + '\Media\zh-tw\bootmgr.efi.mui')
     Write-LogMessage -Message "waiting for copy to complete..."
+    start-sleep -Seconds 3
     While (1 -eq 1) {
         IF (Test-Path $MonitorredFile) {
             #file exists. break loop
             break
         }
         #sleep for 2 seconds, then check again
+        write-host "sleeping"
         Start-Sleep -s 2
     }
 
@@ -262,30 +281,26 @@ If (!(test-path "C:\Program Files (x86)\Windows Kits\10\Assessment and Deploymen
     $7=('/Image:' + $TargetDirectory + '\mount /Add-Package /PackagePath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-StorageWMI.cab"') 
 
     Write-LogMessage -Message "Mounting the WinPE image"
-    Start-Process 'DISM' -ArgumentList $1 -WindowStyle Minimized
+    Start-Process 'DISM' -ArgumentList $1 
 
-    #Monitoring winpe.jpg file to see if mount succeeded
-    $MonitorredFile=($TargetDirectory + '\mount\Windows\System32\winpe.jpg')
-    While (1 -eq 1) {
-        IF (Test-Path $MonitorredFile) {
-            #file exists. break loop
-            break
-        }
-        #sleep for 2 seconds, then check again
-        Start-Sleep -s 2
-    }
+#Need to check if c:\windows\system32\dism.exe is still running
+    While (Get-Process | where-object {$_.ProcessName -Contains "Dism"}) {
+        write-host "." -NoNewline
+        start-sleep -s 2
+    }    
+    write-host ""
     Write-LogMessage -Message "Adding WMI repository"
-    Start-Process 'DISM' -wait -ArgumentList $2 -WindowStyle Minimized
+    Start-Process 'DISM' -wait -ArgumentList $2 
     Write-LogMessage -Message "Adding Network repository"
-    Start-Process 'DISM' -wait -ArgumentList $3 -WindowStyle Minimized
+    Start-Process 'DISM' -wait -ArgumentList $3 
     Write-LogMessage -Message "Adding Scriptig repository"
-    Start-Process 'DISM' -wait -ArgumentList $4 -WindowStyle Minimized
+    Start-Process 'DISM' -wait -ArgumentList $4 
     Write-LogMessage -Message "Adding Powershell repository"
-    Start-Process 'DISM' -wait -ArgumentList $5 -WindowStyle Minimized
+    Start-Process 'DISM' -wait -ArgumentList $5 
     Write-LogMessage -Message "Adding DISM repository"
-    Start-Process 'DISM' -wait -ArgumentList $6 -WindowStyle Minimized
+    Start-Process 'DISM' -wait -ArgumentList $6 
     Write-LogMessage -Message "Adding Storage WMI repository"
-    Start-Process 'DISM' -wait -ArgumentList $7 -WindowStyle Minimized
+    Start-Process 'DISM' -wait -ArgumentList $7
 
 #Need to copy all the required files from github to temp
     Write-LogMessage -Message "Downloading scripts from GitHub"
@@ -304,89 +319,95 @@ If (!(test-path "C:\Program Files (x86)\Windows Kits\10\Assessment and Deploymen
         DownloadWithRetry -url $uri -downloadLocation $outfile -retries 3
         $DownloadedFile=Get-FileContents $outfile
 
+        
 #Copy the files to the mounted image
-If (test-path ($TargetDirectory + "\mount\Windows")) {
-    Write-LogMessage -Message "Copying files to the mounted image"
-        If (test-path ($env:TEMP + '\' + 'PrepareAzureStackPOC.ps1')) {
-            Copy-Item ($env:TEMP + '\' + 'PrepareAzureStackPOC.ps1') ($TargetDirectory + '\mount\PrepareAzureStackPOC.ps1') -Force
-        }
-        If (test-path ($env:TEMP + '\' + 'PrepareAzureStackPOC.psm1')) {
-            Copy-Item ($env:TEMP + '\' + 'PrepareAzureStackPOC.psm1') ($TargetDirectory + '\mount\PrepareAzureStackPOC.psm1') -Force
-        }
-#need to take ownership of WinPE and delete it (to change background later on)
-Write-LogMessage -Message "Taking ownership of background image"
-    $BackgroundImage=($TargetDirectory + '\mount\Windows\System32\winpe.jpg')
-    $acl=Get-Acl $BackgroundImage
-    $Group = New-Object System.Security.Principal.NTAccount("Builtin", "Administrators")
-    $ACL.SetOwner($Group)
-    Set-Acl -Path $BackgroundImage -AclObject $acl
+    If (test-path ($TargetDirectory + "\mount\Windows")) {
+        Write-LogMessage -Message "Copying files to the mounted image"
+            If (test-path ($env:TEMP + '\' + 'PrepareAzureStackPOC.ps1')) {
+                $target=($TargetDirectory + '\mount\PrepareAzureStackPOC.ps1').Replace(" ","")
+                write-host $target
+                Copy-Item ($env:TEMP + '\' + 'PrepareAzureStackPOC.ps1') $target -Force
+            }
+            If (test-path ($env:TEMP + '\' + 'PrepareAzureStackPOC.psm1')) {
+                $target=($TargetDirectory + '\mount\PrepareAzureStackPOC.psm1').replace(" ","")
+                write-host $target
+                Copy-Item ($env:TEMP + '\' + 'PrepareAzureStackPOC.psm1') $target -Force
+            }
+    #need to take ownership of WinPE and delete it (to change background later on)
+    Write-LogMessage -Message "Taking ownership of background image"
+        $BackgroundImage=($TargetDirectory + '\mount\Windows\System32\winpe.jpg').Replace(" ","")
+        Write-host "!!!!" $BackgroundImage
+        $acl=Get-Acl $BackgroundImage
+        $Group = New-Object System.Security.Principal.NTAccount("Builtin", "Administrators")
+        $ACL.SetOwner($Group)
+        Set-Acl -Path $BackgroundImage -AclObject $acl
 
-#Delete the old WinPE.jpg and copy the new one
-Write-LogMessage -Message "Taking full control of background image for replacement"
-    $permission = ".\Administrators","FullControl","Allow"
-    $accessRule = new-object System.Security.AccessControl.FileSystemAccessRule $permission
-    $acl.SetAccessRule($accessRule)
-    Set-Acl -Path $BackgroundImage -AclObject $acl
-    remove-item $BackgroundImage
-    If (test-path ($env:TEMP + '\' + 'winpe.jpg')) {
-        Write-LogMessage -Message "Replacing background image"
-        Copy-Item ($env:TEMP + '\' + 'winpe.jpg') ($BackgroundImage) -Force
+    #Delete the old WinPE.jpg and copy the new one
+    Write-LogMessage -Message "Taking full control of background image for replacement"
+        $permission = ".\Administrators","FullControl","Allow"
+        $accessRule = new-object System.Security.AccessControl.FileSystemAccessRule $permission
+        $acl.SetAccessRule($accessRule)
+        Set-Acl -Path $BackgroundImage -AclObject $acl
+        remove-item $BackgroundImage
+        If (test-path ($env:TEMP + '\' + 'winpe.jpg')) {
+            Write-LogMessage -Message "Replacing background image"
+            Copy-Item ($env:TEMP + '\' + 'winpe.jpg') ($BackgroundImage) -Force
+        }
+
+    #Setting the autostart to run powershell start.ps1 script
+    Write-LogMessage -Message "Setting the autostart scripts"
+        $startnet = Get-Content ($TargetDirectory + '\mount\windows\system32\startnet.cmd')
+        $startnet += "`r`npowershell -c Set-ExecutionPolicy Unrestricted -Force"
+        $startnet += "`r`ncd\"
+        $startnet += "`r`npowershell -NoExit -c X:\Start.ps1"
+        #$startnet
+        Set-Content -Value $startnet -Path ($TargetDirectory +  "\mount\windows\system32\startnet.cmd") -Force
     }
 
-#Setting the autostart to run powershell start.ps1 script
-Write-LogMessage -Message "Setting the autostart scripts"
-    $startnet = Get-Content ($TargetDirectory + '\mount\windows\system32\startnet.cmd')
-    $startnet += "`r`npowershell -c Set-ExecutionPolicy Unrestricted -Force"
-    $startnet += "`r`ncd\"
-    $startnet += "`r`npowershell -NoExit -c X:\Start.ps1"
-    #$startnet
-    Set-Content -Value $startnet -Path ($TargetDirectory +  "\mount\windows\system32\startnet.cmd") -Force
-}
-
-#Closing the mount and making an ISO out of it.....
-    $DISM=('/unmount-image /mountdir:' + $TargetDirectory + '\mount /commit')
-    Write-LogMessage -Message "Unmounting the image"
-    Start-process 'Dism' -ArgumentList $DISM -Wait -WindowStyle Minimized
-    
-    $ISO=('MakeWinPEMedia /ISO ' + $TargetDirectory + ' ' + $TargetDirectory + '\WinPE_ASDK_Stack.iso')
-    If (test-path "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat") {
-        copy-item "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat" $ClosingISOBatchFile
-            $ISOfile = Get-Content $ClosingISOBatchFile
-            $ISOfile += "`r`nREM Creating ISO"
-            $ISOfile += "`r`n$ISO"
-            Set-Content -Value $ISOfile -Path $ClosingISOBatchFile -Force
-        Write-LogMessage -Message "Creating the ISO image"
-        $MonitorredFile=($TargetDirectory + '\WinPE_ASDK_Stack.iso')
-        If (test-path $MonitorredFile) {
-            Write-LogMessage "ISO is already present.."
-        } Else {        
-            Start-Process 'C:\WINDOWS\system32\cmd.exe' -argumentlist "/k $ClosingISOBatchFile" -Verb runAs -WindowStyle Minimized
-        }
-}
-
-$MonitorredFile=($TargetDirectory + '\WinPE_ASDK_Stack.iso')
-Write-LogMessage -Message "waiting for ISO to be created"
-While (1 -eq 1) {
-    IF (Test-Path $MonitorredFile) {
-        #file exists. break loop
-        break
+    #Closing the mount and making an ISO out of it.....
+        $DISM=('/unmount-image /mountdir:' + $TargetDirectory + '\mount /commit')
+        Write-LogMessage -Message "Unmounting the image"
+        Start-process 'Dism' -ArgumentList $DISM -Wait -WindowStyle Minimized
+        
+        $ISO=('MakeWinPEMedia /ISO ' + $TargetDirectory + ' ' + $TargetDirectory + '\WinPE_ASDK_Stack.iso')
+        If (test-path "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat") {
+            copy-item "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat" $ClosingISOBatchFile
+                $ISOfile = Get-Content $ClosingISOBatchFile
+                $ISOfile += "`r`nREM Creating ISO"
+                $ISOfile += "`r`n$ISO"
+                Set-Content -Value $ISOfile -Path $ClosingISOBatchFile -Force
+            Write-LogMessage -Message "Creating the ISO image"
+            $MonitorredFile=($TargetDirectory + '\WinPE_ASDK_Stack.iso')
+            If (test-path $MonitorredFile) {
+                Write-LogMessage "ISO is already present.."
+            } Else {        
+                Start-Process 'C:\WINDOWS\system32\cmd.exe' -argumentlist "/k $ClosingISOBatchFile" -Verb runAs -WindowStyle Minimized
+            }
     }
-    #sleep for 2 seconds, then check again
-    Start-Sleep -s 4
-}
 
-If (test-path ($TargetDirectory + '\WinPE_ASDK_Stack.iso')) {
-    Write-LogMessage -Message "Iso successfully created"
-    $ISOOutput=($TargetDirectory + '\WinPE_ASDK_Stack.iso')
-    Write-Host "      ***********************************************" -foregroundColor Yellow
-    write-host "      please check $ISOOutput " -foregroundColor Yellow
-    Write-Host "      ***********************************************" -foregroundColor Yellow
-    write-host ""
-    
-}
-#Dism /unmount-image /mountdir:D:\winpe_amd71\mount /commit
-#MakeWinPEMedia /ISO D:\winpe_amd71 D:\winpe_amd71\WinPE_ASDK_Stack.iso
-#MakeWinPEMedia /UFD D:\winpe_amd71 P:
+    $MonitorredFile=($TargetDirectory + '\WinPE_ASDK_Stack.iso')
+    Write-LogMessage -Message "waiting for ISO to be created"
+    While (1 -eq 1) {
+        IF (Test-Path $MonitorredFile) {
+            #file exists. break loop
+            break
+        }
+        #sleep for 2 seconds, then check again
+        Start-Sleep -s 4
+    }
+
+    If (test-path ($TargetDirectory + '\WinPE_ASDK_Stack.iso')) {
+        Write-LogMessage -Message "Iso successfully created"
+        $ISOOutput=($TargetDirectory + '\WinPE_ASDK_Stack.iso')
+        Write-Host "      ***********************************************" -foregroundColor Yellow
+        write-host "      please check $ISOOutput " -foregroundColor Yellow
+        Write-Host "      ***********************************************" -foregroundColor Yellow
+        write-host ""
+        
+    }
+    #Dism /unmount-image /mountdir:D:\winpe_amd71\mount /commit
+    #MakeWinPEMedia /ISO D:\winpe_amd71 D:\winpe_amd71\WinPE_ASDK_Stack.iso
+    #MakeWinPEMedia /UFD D:\winpe_amd71 P:
 
 
 
